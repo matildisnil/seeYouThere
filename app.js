3c13537d0c588d1d0de44b6e6b6478b902c63525
Matilda Lindahl <99189688+matildisnil@users.noreply.github.com>
2022-11-24 20:26:00 +0100

    installed abort-controller, seems to have been causing issue

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
 const express = require('express');
 const cors = require("cors");
 const app = express();
-const controller = require("./controller.js");
-// require('dotenv').config();
+const { trips } = require("./controller.js");
+require('dotenv').config();
 
 app.use(express.static('static'));
 express.urlencoded({ extended: false });
 
-// if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
-//   const whitelist = ["http://localhost:3000"]
-//   const corsOptions = {
-//     origin: function (origin, callback) {
-//       if (!origin || whitelist.indexOf(origin) !== -1) {
-//         callback(null, true)
-//       } else {
-//         callback(new Error("Not allowed by CORS"))
-//       }
-//     },
-//     credentials: true,
-//   }
-//   app.use(cors(corsOptions));
-// }
+if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
+  const whitelist = ["http://localhost:3000"]
+  const corsOptions = {
+    origin: function (origin, callback) {
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+        callback(null, true)
+      } else {
+        callback(new Error("Not allowed by CORS"))
+      }
+    },
+    credentials: true,
+  }
+  app.use(cors(corsOptions));
+}
 
-app.get('/api/trips', controller.trips);
+app.get('/api/trips', trips);
 
 app.get('/test',(req,res) => {
   res.json({'message': 'hello'});
