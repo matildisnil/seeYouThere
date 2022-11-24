9ff27de4a8a01f54ddb86fdd40d62f8c7e95fc00
Matilda Lindahl <99189688+matildisnil@users.noreply.github.com>
2022-11-24 19:57:06 +0100

    testing to only serve test-route

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
 const express = require('express');
-const cors = require("cors");
-const app = express();
-const controller = require("./controller.js");
-require('dotenv').config();
+// const cors = require("cors");
+// const app = express();
+// const controller = require("./controller.js");
+// require('dotenv').config();
 
-// app.use(express.static('static'));
-express.urlencoded({ extended: false });
+// // app.use(express.static('static'));
+// express.urlencoded({ extended: false });
 
-if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
-  const whitelist = ["http://localhost:3000"]
-  const corsOptions = {
-    origin: function (origin, callback) {
-      if (!origin || whitelist.indexOf(origin) !== -1) {
-        callback(null, true)
-      } else {
-        callback(new Error("Not allowed by CORS"))
-      }
-    },
-    credentials: true,
-  }
-  app.use(cors(corsOptions));
-}
+// if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
+//   const whitelist = ["http://localhost:3000"]
+//   const corsOptions = {
+//     origin: function (origin, callback) {
+//       if (!origin || whitelist.indexOf(origin) !== -1) {
+//         callback(null, true)
+//       } else {
+//         callback(new Error("Not allowed by CORS"))
+//       }
+//     },
+//     credentials: true,
+//   }
+//   app.use(cors(corsOptions));
+// }
 
-app.get('/api/trips', controller.trips);
+// app.get('/api/trips', controller.trips);
+
+app.get('/',(req,res) => {
+  res.json({'message': 'hello'});
+})
 
 module.exports.app = app;
